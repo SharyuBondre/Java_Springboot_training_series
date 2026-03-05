e39d10abdbbfc9381e10e9c6195c51a998bae9b0
Your Name <you@example.com>
2026-03-05 18:55:57 +0530

    2nd

---
+++ src/Count_Freq_Of_ArrayElements/FrequencyExample.java
@@ -0,0 +1,27 @@
+package Count_Freq_Of_ArrayElements;
+
+public class FrequencyExample {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 4, 1, 2};
+        boolean[] visited = new boolean[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (visited[i] == true) {
+                continue;
+            }
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " occurs " + count + " times");
+        }
+    }
+}
