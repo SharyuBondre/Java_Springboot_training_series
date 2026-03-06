8136f3acdcf48874c682bffec83658bd153be9ff
Sharyu Bondre <75156818+SharyuBondre@users.noreply.github.com>
2026-03-06 12:03:48 +0530

    Update FrequencyExample.java

--- src/Count_Freq_Of_ArrayElements/FrequencyExample.java
+++ src/Count_Freq_Of_ArrayElements/FrequencyExample.java
@@ -4,7 +4,7 @@ public class FrequencyExample {
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 2, 4, 1, 2};
-        boolean[] visited = new boolean[arr.length];
+        boolean[] visited = new boolean[arr.length]; //flase
 
         for (int i = 0; i < arr.length; i++) {
 
