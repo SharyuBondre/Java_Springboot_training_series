1fc1f446e44894784eeb1a970a0e48cca2a38d4f
Your Name <you@example.com>
2026-03-06 14:47:48 +0530

    4th

---
+++ src/Abstractions/TestVehicle.java
@@ -0,0 +1,29 @@
+package Abstractions;
+
+abstract class Vehicle {
+
+    abstract void start();
+}
+
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car starts with key");
+    }
+}
+
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("Bike starts with self start");
+    }
+}
+
+public class TestVehicle {
+    public static void main(String[] args) {
+
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+
+        v1.start();
+        v2.start();
+    }
+}
